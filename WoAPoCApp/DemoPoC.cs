f9b74047fbe626257e556ebe4ce14154c948e75a
Jorge <jcerdas25@gmail.com>
2024-06-14 15:42:27 -0600

    Setting up DemoPoc class, this where all the logic for the demo app will be

---
+++ WoAPoCApp/DemoPoC.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WoAPoCApp
+{
+    public class DemoPoC
+    {
+        private int[] arr = new int[1000000];
+        public void RunDemo() { }
+
+        public void SetUpArray(int array) { }
+
+        public void FindTargetNumber() { }
+
+        public void RunBinarySearch() { }
+    }
+}
