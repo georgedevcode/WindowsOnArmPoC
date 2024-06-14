4afc9f5b448dd4a492e8c272a00cddfd17b7ff32
Jorge <jcerdas25@gmail.com>
2024-06-14 15:25:30 -0600

    Testing dotNetBenchMark Nugget package

---
+++ WoAPoCApp/BeanchMarkExecutor.cs
@@ -0,0 +1,21 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WoAPoCApp
+{
+    public class BeanchMarkExecutor
+    {
+        [Benchmark]
+        public void Execute() { 
+        
+            var calculator = new Calculador();
+
+            calculator.Add(1, 2);
+
+        }
+    }
+}
---
+++ WoAPoCApp/Calculador.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Diagnostics.Tracing.Parsers.Kernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WoAPoCApp
+{
+    public class Calculador
+    {
+        public int Add(int first, int second) => first + second;
+
+    }
+}
