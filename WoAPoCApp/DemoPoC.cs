53a99e3f34e03d2976547a9c7f4b46404f0c1164
Jorge <jcerdas25@gmail.com>
2024-07-01 15:48:39 -0600

    Working on Sorting algorithm

--- WoAPoCApp/DemoPoC.cs
+++ WoAPoCApp/DemoPoC.cs
@@ -1,4 +1,5 @@
 ﻿using BenchmarkDotNet.Attributes;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,22 +13,30 @@ namespace WoAPoCApp
         public readonly int[] arr = new int[1000000];
         public int RandomNumber = 0;
         private bool FoundTarget = false;
+        private int Target = 0;
+        private int left = 0;
+        private int right = 0;
 
         Random rand = new Random();
         public DemoPoC()
         {
-            SetUpArray(arr);
+            SetUpArray();
 
-            GenerateRandNumber();
+            QuickSort(this.arr, this.left, this.right);
+
+            this.Target = GenerateRandNumber();
         }
 
-        public int[] SetUpArray(int[] arr)
+        [Benchmark]
+        public int[] SetUpArray()
         {
 
-            for (int i = 0; i < arr.Length; i++)
+            for (int i = 0; i < this.arr.Length; i++)
             {
-                this.arr[i] = i;
+                this.arr[i] = GenerateRandNumber();
             }
+
+            this.right = this.arr.Length - 1;
 
             return this.arr;
         }
@@ -38,6 +47,63 @@ namespace WoAPoCApp
             this.RandomNumber = rand.Next(this.arr.Length);
 
             return RandomNumber;
+        }
+
+        public int Partition(int[] arr, int left, int right)
+        {
+            int pivot = arr[left];
+
+            while (true)
+            {
+                while (arr[left] < pivot)
+                {
+                    left++;
+                }
+
+                while (arr[right] > pivot)
+                {
+                    right--;
+                }
+
+                if (left < right)
+                {
+                    if (arr[left] == arr[right]) return right;
+
+                    int temp = arr[left];
+                    arr[left] = arr[right];
+                    arr[right] = temp;
+                }
+                else
+                {
+                    return right;
+                }
+            }
+        }
+
+        // O(N log(N))
+        [Benchmark]
+        public void QuickSort()
+        {
+
+            if (left < right)
+            {
+
+                int pivot = Partition(this.arr, left, right);
+
+                if (pivot > 1)
+                {
+
+                    QuickSort(this.arr, left, right);
+
+                }
+
+                if (pivot + 1 < right)
+                {
+                    QuickSort(this.arr, pivot + 1, right);
+                }
+
+            }
+
         }
 
         [Benchmark]
